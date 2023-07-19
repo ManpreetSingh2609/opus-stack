25e96371c9d2cc57015ecd46e938758cb08fb961
Manpreet Singh <manpreetsingh8bp@gmail.com>
2023-07-19 19:12:30 +0530

    Updated API leakage

--- script/config.js
+++ script/config.js
@@ -1,1 +1,10 @@
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBmVurLeknRCPnyekMBTR1gxsU6PyTEBHo",
+    authDomain: "opusstack.firebaseapp.com",
+    projectId: "opusstack",
+    storageBucket: "opusstack.appspot.com",
+    messagingSenderId: "219330664372",
+    appId: "1:219330664372:web:08c620713c94fa0bf304e1",
+    measurementId: "G-6XBF7KM10T"
+}
+export{firebaseConfig};
